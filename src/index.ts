6bb93fe1aabf0239bae2250809400aa9825bf691
sswq2020 <a309108243@qq.com>
2022-08-31 21:54:48 +0800

    feat: 🎸 全局注册,全局引入,局部引入最基本的实现
     注意组件的name属性,官网有具体解释

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+// import SButton from './button'
+// import SButton from './SFCButton.vue'
+import JSXButton from './JSXButton'
+
+createApp(JSXButton).mount('#app')
